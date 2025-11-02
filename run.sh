70282fb65309cfa29e33c91cc612b1edbf02bee6
Andrii <andrewvezdel@gmail.com>
2025-11-03 00:06:24 +0200

    kernel improvements

--- run.sh
+++ run.sh
@@ -1,15 +1,12 @@
 cargo clean
-# 1️⃣ Build the kernel 
 cargo build -Z build-std=core,alloc,compiler_builtins --target x86_64-duxa_os.json -p kernel
 
-# 2️⃣ Build disk-builder (host target, std)
 cargo run -p disk-builder
 
 qemu-system-x86_64 \
-  -drive format=raw,file=bios_disk.img \
+  -drive format=raw,file=./disk-builder/bios_disk.img \
   -serial stdio \
   -m 512M \
   -boot c \
   -no-reboot \
-  -d int \
   -no-shutdown
