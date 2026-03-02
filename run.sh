944a06de20a723c8eb4e13d5832907ff57bf74b3
Andrii <duxaondablock@proton.me>
2026-03-02 19:27:37 +0200

    came back

--- run.sh
+++ run.sh
@@ -1,12 +1,16 @@
 # cargo clean
-cargo build -Z build-std=core,alloc,compiler_builtins --target x86_64-duxa_os.json -p kernel
+echo "Running kernel compilation"
+cargo build -p kernel -Z build-std=core,alloc,compiler_builtins
 
-cargo run -p disk-builder
-
+echo "Running disk-builder"
+cargo run -p disk-builder --target x86_64-unknown-linux-gnu
+echo "Running QEMU"
 qemu-system-x86_64 \
-  -drive format=raw,file=./disk-builder/bios_disk.img \
-  -serial stdio \
-  -m 4096M \
-  -boot c \
-  -no-reboot \
-  -no-shutdown
+    -drive format=raw,file=./disk-builder/bios_disk.img \
+    -serial stdio \
+    -m 4096M \
+    -boot c \
+    -no-reboot \
+    -no-shutdown \
+    -machine pc \
+    -device isa-debug-exit,iobase=0xf4,iosize=0x04
