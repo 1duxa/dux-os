6a209e99601020d44c830513336c9d970fdc56d4
Andrii <duxaondablock@proton.me>
2026-04-14 19:37:49 +0300

    added small VM support

--- run.sh
+++ run.sh
@@ -1,16 +1,13 @@
-# cargo clean
-echo "Running kernel compilation"
-cargo build -p kernel -Z build-std=core,alloc,compiler_builtins
+cargo build -p kernel -Z build-std=core,alloc,compiler_builtins --release
 
-echo "Running disk-builder"
-cargo run -p disk-builder --target x86_64-unknown-linux-gnu
-echo "Running QEMU"
+cargo run -p disk-builder --target x86_64-unknown-linux-gnu --release
+
 qemu-system-x86_64 \
     -drive format=raw,file=./disk-builder/bios_disk.img \
     -serial stdio \
-    -m 4096M \
+    -m 8096M \
     -boot c \
     -no-reboot \
     -no-shutdown \
     -machine pc \
-    -device isa-debug-exit,iobase=0xf4,iosize=0x04
+    -device isa-debug-exit,iobase=0xf4,iosize=0x04 \
